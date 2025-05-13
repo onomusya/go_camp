65c8addff7fe45770570a8b196bd588c977dc380
onomusya <onomusya@gmail.com>
2025-05-13 16:36:25 +0900

    予約機能の実装

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -4,4 +4,11 @@ class Reservation < ApplicationRecord
 
   validates :start_date, presence: true
   validates :end_date, presence: true
+
+  def calculate_total_price
+    return unless site && start_date && end_date
+    num_days = [(end_date - start_date).to_i, 1].max
+    self.total_price = site.price * num_days
+  end
+
 end
