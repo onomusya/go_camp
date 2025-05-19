3018d397f5c190421acc00865caaaf77a8e974f9
onomusya <onomusya@gmail.com>
2025-05-19 15:17:45 +0900

    予約完了ページの実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'pages/rules'
   get 'accesses/show'
   root to: "sites#index"
+  get 'reservations/complete', to: 'reservations#complete', as: 'reservation_complete'
   get 'sites/introduction', to: 'sites#show', as: 'site'
   get 'access', to: 'accesses#show'
   resources :sites, only: [:index, :show]
